41232cdfb6fd90edcb8d22024e2a5cbbf6cb5e20
renato <renato.ribeiro@synchro.com.br>
2017-06-13 23:43:53 -0300

    arquivos faltando

--- core/src/main/groovy/entidades/Compra.groovy
+++ core/src/main/groovy/entidades/Compra.groovy
@@ -4,14 +4,13 @@ package entidades
  * Representa uma compra
  */
 class Compra {
-    Date dataDaCompra
     List<ItemDaCompra> itens = []
 
-    public boolean adicionarItem(ItemDaCompra itemDaCompra) {
+    boolean adicionarItem(ItemDaCompra itemDaCompra) {
         itens.add(itemDaCompra)
     }
 
-    public BigDecimal subTotal() {
+    BigDecimal subTotal() {
         BigDecimal subTotal = 0
         itens.each {
             subTotal += it.produto.preco * it.quantidade
